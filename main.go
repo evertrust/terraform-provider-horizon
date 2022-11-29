19b8b0d99978a22e07fc5802149d05bf7fca10f7
AdrienDucourthial <adu@evertrust.fr>
2022-11-29 11:26:28 +0100

    test

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
+// Provider documentation generation.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-name horizon
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
