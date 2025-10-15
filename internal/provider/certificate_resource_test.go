767a6d219f5b1bc568217d0e3bdc28c214e45541
Hugo Milair <hugo.milair@gmail.com>
2025-10-15 14:25:36 +0200

    chore(workflow): launch horizon instance for acc tests

--- internal/provider/certificate_resource_test.go
+++ internal/provider/certificate_resource_test.go
@@ -1,15 +1,138 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
 	horizontypes "github.com/evertrust/horizon-go/types"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
+type Credentials struct {
+	Username string
+	Password string
+}
 type CertificateSuite struct {
 	suite.Suite
+	HorizonEndpoint string
+	Credentials     Credentials
+	Provider        schema.Provider
+}
+
+func (suite *CertificateSuite) SetupTest() {
+	creds := Credentials{
+		Username: "test",
+		Password: "test",
+	}
+	suite.HorizonEndpoint = "http://localhost:9000"
+	suite.Credentials = creds
+}
+
+func NewProvider(version string) provider.Provider {
+	prov := New(version)
+	return prov()
+}
+
+var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
+	"horizon": func() (tfprotov6.ProviderServer, error) {
+		return providerserver.NewProtocol6WithError(NewProvider("0.3.1"))()
+	},
+}
+
+func (s *CertificateSuite) TestAcc_Certificate_CRUD() {
+	t := s.T()
+
+	rnd := acctest.RandStringFromCharSet(6, acctest.CharSetAlphaNum)
+	resourceName := "horizon_certificate.test"
+
+	cfgCreate := fmt.Sprintf(`
+provider "horizon" {
+  alias = "with-creds"
+  endpoint = "%s"
+  username = "%s"
+  password = "%s"
+}
+
+resource "horizon_certificate" "test" {
+  provider = horizon.with-creds
+  profile   = "default"
+  labels = [
+    { label = "env", value = "acc-%s" }
+  ]
+  wait_for_third_parties = []
+}
+`, s.HorizonEndpoint, s.Credentials.Username, s.Credentials.Password, rnd)
+
+	cfgUpdate := fmt.Sprintf(`
+provider "horizon" {
+  alias = "with-creds"
+  endpoint = "%s"
+  username = "%s"
+  password = "%s"
+}
+
+resource "horizon_certificate" "test" {
+  provider = horizon.with-creds
+  profile          = "EnrollmentProfile"
+  key_type         = "rsa-2048"
+  revoke_on_delete = true
+  renew_before     = 30
+  subject = [
+    {
+      element = "CN"
+      type    = "CN"
+      value   = "example.com"
+    }
+  ]
+  sans = [
+    {
+      type  = "DNSNAME"
+      value = ["example.com", "www.example.com"]
+    }
+  ]
+  labels = [
+    { label = "env", value = "acc-%s" },
+    { label = "team", value = "platform" },
+  ]
+  wait_for_third_parties = []
+}
+`, s.HorizonEndpoint, s.Credentials.Username, s.Credentials.Password, rnd)
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: cfgCreate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttr(resourceName, "profile", "default"),
+					resource.TestCheckResourceAttr(resourceName, "self_signed", "false"),
+				),
+			},
+			{
+				Config: cfgUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "profile", "default"),
+					resource.TestCheckTypeSetElemNestedAttrs(resourceName, "labels.*", map[string]string{
+						"label": "team",
+						"value": "platform",
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
 }
 
 func (s *CertificateSuite) TestHasThirdParties_AllPresent() {
